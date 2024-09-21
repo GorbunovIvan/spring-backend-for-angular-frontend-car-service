17883652fa1b90c3f39f9bacc0d92e12630b98d7
GorbunovIvan <110302058+GorbunovIvan@users.noreply.github.com>
2024-09-21 14:38:28 +0300

    init

--- src/main/java/org/example/SpringBackendForAngularFrontendCarServiceApplication.java
+++ src/main/java/org/example/SpringBackendForAngularFrontendCarServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBackendForReactJsFrontendCarServiceApplication {
+public class SpringBackendForAngularFrontendCarServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBackendForReactJsFrontendCarServiceApplication.class, args);
+        SpringApplication.run(SpringBackendForAngularFrontendCarServiceApplication.class, args);
     }
 
 }
